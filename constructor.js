92e7de8d0d78ad928afd96bc71e55cee12c47075
asadhijj <hijjawi30@gmail.com>
2022-08-10 05:22:22 +0300

    done

--- constructor.js
+++ constructor.js
@@ -1,11 +1,11 @@
 'use strict';
 var id=1000;
 
+
 const allFood =[];
 
-function Food ( foodIDValue, foodNVlaue, typeValue, moneyValue) {
- this.id= id++;
- this.foodID = foodIDValue;
+function Food ( foodNVlaue, typeValue, moneyValue) {
+ this.foodID =  id++;
  this.foodN = foodNVlaue;
  this.type = typeValue;
  this.money = moneyValue;
@@ -15,24 +15,32 @@ allFood.push(this);
 
 
  Food.prototype.render = function () {
-    let table= document.getElementById('table');
+    let table = document.getElementById("table");
 
-    let row=document.createElement('tr');
-    table.appendChild(row);
+    let row = document.createElement("tr");
+    row.setAttribute("class", "data");
 
-    let id=document.createElement('td');
-    id.textContent=`${this.foodID}`;
+    let data = document.createElement("td");
+    data.setAttribute("class", "data");
+    data.textContent = `${this.foodID}`;
+    row.appendChild(data); 
 
-    let name=document.createElement('td')
-    name.textContent=`${this.foodN}`;
+    let dataN = document.createElement("td");
+    dataN.setAttribute("class", "data");
+    dataN.textContent = `${this.foodN}`;
+    row.appendChild(dataN); 
 
-    let type=document.createElement('td')
-    type.textContent=`${this.type}`;
+    let dataR = document.createElement("td");
+    dataR.setAttribute("class", "data");
+    dataR.textContent = `${this.type}`;
+    row.appendChild(dataR); 
 
-    let price=document.createElement('td')
-    price.textContent=`${this.money}`;
+    let dataP = document.createElement("td");
+    dataP.setAttribute("class", "data");
+    dataP.textContent = `${this.money} JD`;
+    row.appendChild(dataP);
 
-    row.appendChild(id,name,type,price)    
+    table.appendChild(row); 
 }
 
 let form=document.getElementById('form');
